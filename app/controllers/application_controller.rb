17e82798e9a7f77276b752e1ff7ee790db6c7506
aldosolorzano <aldosolorzanop@gmail.com>
2017-03-26 23:14:33 -0700

    add @song in the share_controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,33 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
+  def get_song
+    @song
+  end
+
+  def search_cache_query search_term
+    query = SearchCache.where("search_term ILIKE ?", "%#{search_term}%")
+    if query.empty?
+      @song = connect_spotify search_term
+      redirect_to new_share_path({current_song:@song})
+    else
+      @song = JSON.parse query.first.result
+      redirect_to new_share_path({current_song:@song})
+    end
+  end
+
+
+  def connect_spotify search_term
+    response = RSpotify::Track.search(search_term)
+    # byebug
+    json_response = response.first.to_json
+
+    if json_response != "null"
+      SearchCache.create(search_term:search_term, result:json_response)
+      JSON.parse json_response
+    end
+  end
+
   def current_user
     User.find session[:user_id] if user_signed_in?
   end
--- app/controllers/shares_controller.rb
+++ app/controllers/shares_controller.rb
@@ -1,13 +1,14 @@
 class SharesController < ApplicationController
   before_action :authenticate_user!
   before_action :authorize,only:[:edit,:update]
-
+  # before_action :get_song,only:[:new]
   def index
     @shared_songs = current_user.shared_songs
     @shares = current_user.shares.order(created_at: :desc)
   end
 
   def new
+    @song = params[:current_song]
     @share = Share.new
   end
 
@@ -16,37 +17,16 @@ class SharesController < ApplicationController
   end
 
   def create
-    # share_params = params.require(:share).permit(:title, :artist,{shared_user_ids:[]})
-    # @share = Share.new share_params
-    # @share.user = current_user
-    # @share.song = Song.find_by(name: params[:title])
-    #
-    # if @share.save
-    #   redirect_to root_path, notice:'Share created'
-    #
-    # else
-    #   render :new
-    #
-    # end
+    share_params = params.require(:share).permit(:title, :artist,{shared_user_ids:[]})
+    @share = Share.new share_params
+    @share.user = current_user
+    @share.song = Song.find_by(name: params[:title])
 
-    @share = Share.new
-    search_term = params[:share][:artist]
-    search_cache_query = SearchCache.where("search_term ILIKE ?", "%#{search_term}%")
-    if search_cache_query.empty?
-      response = RSpotify::Track.search(search_term)
-      json_response = response.first.to_json
-
-      if json_response != "null"
-        SearchCache.create(search_term:search_term, result:json_response)
-        @song = JSON.parse json_response
-      end
-
+    if @share.save
+      redirect_to root_path, notice:'Share created'
     else
-      @song = JSON.parse search_cache_query.first.result
+      render :new
     end
-
-    # render json: @song
-    render :new
   end
 
   def update
