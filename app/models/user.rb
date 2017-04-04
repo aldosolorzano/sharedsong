8c3571c298e96f2ec34f427bfbfbccd0b551446a
aldosolorzano <aldosolorzanop@gmail.com>
2017-04-04 14:02:53 -0700

    notification panel showing pending shares

--- app/models/user.rb
+++ app/models/user.rb
@@ -48,10 +48,33 @@ class User < ApplicationRecord
             user_creator: user_creator.first_name,
             song_title: s.title,
             artist: s.artist
-            })
+          })
         end
       end
       pending_shares
   end
+  def create_array_of_accepted_shares
+      accepted_shares = []
+      self.shared_songs.each do |s|
+        if s.aasm_state == "accepted"
+          user_creator = User.find(s.user_id)
+          accepted_shares.push({
+            user_creator: user_creator.full_name,
+            song_title: s.title,
+            artist: s.artist,
+            shared_users: array_of_shared_users(s)
+          })
+        end
+      end
+      accepted_shares
+  end
 
-end
+  def array_of_shared_users share
+    array_shared_users = []
+    share.shared_users.each do |u|
+      array_shared_users.push(u.first_name.capitalize)
+    end
+    array_shared_users
+  end
+
+  end
