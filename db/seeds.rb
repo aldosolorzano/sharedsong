bfce997091dcfcd00f8c2c49eb31b1abd7524409
aldosolorzano <aldosolorzanop@gmail.com>
2017-05-01 16:26:54 -0700

    bonsai.rb and cluster

--- db/seeds.rb
+++ db/seeds.rb
@@ -12,13 +12,13 @@ puts "#{Song.count} songs were created"
 
 # seed users
 
-100.times do
-  User.create(
-    first_name: Faker::Name.first_name,
-    last_name:  Faker::Name.last_name,
-    email:      Faker::Internet.email,
-    password:   'abc'
-  )
-end
-
-puts "#{User.count} users were created"
+# 100.times do
+#   User.create(
+#     first_name: Faker::Name.first_name,
+#     last_name:  Faker::Name.last_name,
+#     email:      Faker::Internet.email,
+#     password:   'abc'
+#   )
+# end
+#
+# puts "#{User.count} users were created"
